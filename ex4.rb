9539bad810b9f6a8b895c3453b3cb9f7bcf5dd17
Marcos V. Pagelkopf Jr <marcos@marcosvpj.com.br>
2016-03-16 19:56:50 -0300

    Using variables

---
+++ ex4.rb
@@ -0,0 +1,13 @@
+puts "Now using variables!!!\n\n"
+
+cars = 5
+drivers = 5
+passangers = 7
+
+speed_limit = 50
+
+puts "The speed limit is #{speed_limit} Km/h. \n\n"
+
+puts "We have #{cars} cars."
+puts "And #{passangers} passangers."
+puts "Wich gives an average of #{(passangers + drivers) / cars} person per car."
