f63f36977be6a472d5235c882a8e4679823f6c0d
Marcos V. Pagelkopf Jr <marcos@marcosvpj.com.br>
2016-03-16 19:58:38 -0300

    Exercise 4 - More variables

--- ex4.rb
+++ ex4.rb
@@ -3,6 +3,7 @@ puts "Now using variables!!!\n\n"
 cars = 5
 drivers = 5
 passangers = 7
+total_passangers = drivers + passangers
 
 speed_limit = 50
 
@@ -11,3 +12,4 @@ puts "The speed limit is #{speed_limit} Km/h. \n\n"
 puts "We have #{cars} cars."
 puts "And #{passangers} passangers."
 puts "Wich gives an average of #{(passangers + drivers) / cars} person per car."
+puts "And a total of #{total_passangers} people."
