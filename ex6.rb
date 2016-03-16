15561868d58cc0ca6e2d9debf04b6edec6a2a67a
Marcos V. Pagelkopf Jr <marcos@marcosvpj.com.br>
2016-03-16 20:11:13 -0300

    Exercise 6 - Striings and Jokes

---
+++ ex6.rb
@@ -0,0 +1,20 @@
+puts "Exercise 6 - More strings and jokes :)\n\n\n"
+
+types_of_people = 10
+binary = 'binary'
+
+first_line = "There are #{types_of_people} types of people."
+second_line = "Those who know #{binary} and those who don't!"
+
+puts first_line
+puts second_line
+
+funny = false
+
+puts "Funny? #{funny}\n\n\n"
+
+right = "Right side"
+left = "Left side"
+
+puts "And I can even 'sum' strings:"
+puts left + " and " + right
