e241b9768085bf02d7fb66fa91a0c6e354bdd722
Marcos V. Pagelkopf Jr <marcos@marcosvpj.com.br>
2016-03-16 20:03:52 -0300

    Exercise 5 - More variables and strings

---
+++ ex5.rb
@@ -0,0 +1,10 @@
+puts "Exercise 5 - More variables an strings!\n\n"
+
+name = "Dino"
+last_name = "Sauro"
+
+puts "The name is #{name} #{last_name}.\n\n"
+
+age = 107
+
+puts "And h is really old, #{age} years old to be precise."
