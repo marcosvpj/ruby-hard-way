cc614150f21eceae78469afcc5f3156fa17a015b
Marcos V. Pagelkopf Jr <marcos@marcosvpj.com.br>
2016-03-16 20:42:44 -0300

    Exercise 12 - Prompting people numbers

---
+++ ex12.rb
@@ -0,0 +1,7 @@
+puts "Exercise 12 - Prompting people number\n\n"
+
+print "Give me a number: "
+number = gets.chomp.to_i
+
+puts "The number is #{number}"
+puts "I can even do math with: #{number * 2}"
