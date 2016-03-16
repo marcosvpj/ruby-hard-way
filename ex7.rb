eaf7cfb3705007d0e7fd4248b16eb692558558a2
Marcos V. Pagelkopf Jr <marcos@marcosvpj.com.br>
2016-03-16 20:18:03 -0300

    Exercise 7 - More printing

---
+++ ex7.rb
@@ -0,0 +1,16 @@
+puts "Exercise 7 - More printing\n\n\n"
+
+puts "Beer " * 3
+
+puts "\n\nI like #{'beer'} :)"
+
+puts "and"
+
+end1 = 'P'
+end2 = 'i'
+end3 = 'z'
+end4 = 'z'
+end5 = 'a'
+
+print end1 + end2
+puts end3 + end4 + end5
