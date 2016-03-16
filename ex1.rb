d983cafecc3763a7de7fe91790530491c0df823e
Marcos V. Pagelkopf Jr <marcos@marcosvpj.com.br>
2016-03-16 19:42:33 -0300

    Quotes and double quotes

--- ex1.rb
+++ ex1.rb
@@ -5,3 +5,7 @@ puts "First exercise of Ruby the hard way"
 # UTF-8 texts
 puts "Texto com caracteres especiais:"
 puts "Paçoca e cachaça"
+
+#Quotes and double quotes
+puts "Testing 'quotes' and \"double qoutes\""
+puts 'Testing \'quotes\' and "double qoutes"'
